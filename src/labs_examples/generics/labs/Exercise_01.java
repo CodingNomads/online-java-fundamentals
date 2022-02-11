c2e60e0d826dfad899cf3dc1127a73ec0c8db8f1
NikitaBaranov-Koho <nikita@koho.ca>
2022-02-10 19:55:33 -0500

    Solution to lab generics 1

--- src/labs_examples/generics/labs/Exercise_01.java
+++ src/labs_examples/generics/labs/Exercise_01.java
@@ -2,9 +2,55 @@ package labs_examples.generics.labs;
 
 /**
  * Generics Exercise 1:
- *
- *      1.) Write a generic class with at least two instance variables, a constructor, and getter and setter methods.
- *      2.) Create a few objects of your generic class with different data types to demonstrate it's
- *          dynamic usage.
+ * <p>
+ * 1.) Write a generic class with at least two instance variables, a constructor, and getter and setter methods.
+ * 2.) Create a few objects of your generic class with different data types to demonstrate its dynamic usage.
  */
 
+class GenericTuple<TL, TR> {
+
+    private TL left;
+    private TR right;
+
+    public GenericTuple(TL left, TR right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public TL getLeft() {
+        return left;
+    }
+
+    public void setLeft(TL left) {
+        this.left = left;
+    }
+
+    public TR getRight() {
+        return right;
+    }
+
+    public void setRight(TR right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "GenericTuple{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
+
+class GenericsLab1 {
+
+    public static void main(String[] args) {
+
+        GenericTuple<String, Integer> age = new GenericTuple<>("age", 18);
+        GenericTuple<Integer, Integer> range = new GenericTuple<>(5, 20);
+
+        System.out.println(age);
+        System.out.println(range);
+
+    }
+}
