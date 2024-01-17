58cbeda6ca36bfe3618d40ffca3768628cb6b4d9
CodingNomads Dev <ryan@codingnomads.co>
2024-01-17 12:06:52 -0800

    Update CustomQueue.java

--- src/labs_examples/datastructures/queue/examples/CustomQueue.java
+++ src/labs_examples/datastructures/queue/examples/CustomQueue.java
@@ -1,60 +1,49 @@
 package labs_examples.datastructures.queue.examples;
 
-import labs_examples.datastructures.linkedlist.examples.CustomLinkedList;
-
-/**
- * Created by Kevin Graham - https://codingnomads.co
- */
 // Queue is generic so it can hold any object type
 public class CustomQueue<V> {
+  // create a LinkedList for to use as 
+  // the underlying data structure
+  // keep this private so it can't be modified
+  // outside of this class
+  private LinkedList<V> list = new LinkedList<>();
 
-    // create a LinkedList for us to use as the underlying data structure
-    private CustomLinkedList<V> list = new CustomLinkedList<>();
+  /**
+   * Adds an item to the Queue
+   * @param item to be added
+   */
+  public void enqueue(V item) {
+    // insert item into front of list
+    list.addLast(item);
+  }
 
-    // simple boolean to track whether or not the queue is empty
-    private boolean isEmpty;
-
-    // constructor - set empty to true upon creation
-    public CustomQueue() {
-        isEmpty = true;
+  /**
+   * Removes an item from the Queue
+   * @return the removed item
+   */
+  public V dequeue() {
+    if (list.isEmpty()){
+      return null;
+      // you could also throw an exception here
     }
+    try {
+      // get item from the front of the Queue
+      V item = list.removeFirst();
 
-    /**
-     * Adds an item to the Queue
-     * @param item to be added
-     */
-    public void enqueue(V item) {
-        // insert item into front of list
-        list.add(item);
-        isEmpty = false;
+      // return first item that you got just above
+      return item;
+    } catch (NullPointerException ex) {
+      return null;
     }
+  }
 
-    /**
-     * Removes an item from the Queue
-     * @return the removed item
-     */
-    public V dequeue() {
-        try {
-            // get item from the front of the Queue
-            V item = list.get(0);
-
-            // remove that item from the front of the Queue
-            list.remove(0);
-
-            // check if empty
-            if (list.get(0) == null) {
-                isEmpty = true;
-            }
-
-            // return first item we got just above
-            return item;
-
-        } catch (NullPointerException ex) {
-            return null;
-        }
-    }
-
-    public boolean isEmpty() {
-        return isEmpty;
-    }
-}
+   /**
+   * Removes true if the Queue is empty false
+   * if the queue is not empty
+   * @return boolean isEmpty
+   */
+  public boolean isEmpty(){
+    // surface this method to be called for functionality
+    return list.isEmpty();
+  }
+}	
