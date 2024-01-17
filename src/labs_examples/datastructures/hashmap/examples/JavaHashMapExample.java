6d7a50ad202e6ea78ea68bf398a2dc1c978003ef
CodingNomads Dev <ryan@codingnomads.co>
2024-01-17 13:13:31 -0800

    Update JavaHashMapExample.java

--- src/labs_examples/datastructures/hashmap/examples/JavaHashMapExample.java
+++ src/labs_examples/datastructures/hashmap/examples/JavaHashMapExample.java
@@ -8,37 +8,35 @@ import java.util.Set;
 /**
  * Created by ryandesmond - https://codingnomads.co
  */
-public class Main {  
-  public static void main(String[] args){
-    // create the hashmap  
-    // the "key" will be the email, a String
-    // and the "value" will the Person object
-    HashMap<String, Integer> capitalCityPopulations = new HashMap();
-  
-    // "put" a few capital cities with their population
-    capitalCityPopulations.put("Ottawa", 1077900);
-    capitalCityPopulations.put("Lima", 11204000);
-    capitalCityPopulations.put("Nairobi", 5541172);
+public class JavaHashMapExample {
 
-    // demonstrate "getting" an 
-    // element out of the HashMap
-    int limaPopulation = 
-      capitalCityPopulations.get("Lima");
-    System.out.println(limaPopulation);
+    public static void main(String[] args){
+        // create the hashmap
+        // the "key" will be the email, a String
+        // and the "value" will the Person object
+        HashMap<String, Integer> capitalCityPopulations = new HashMap();
 
-    // demonstrate iterating through 
-    // the entries of a HashMap
-    Set entries = capitalCityPopulations.entrySet();
-    Iterator iterator = entries.iterator();
-    // loop through the entries
-    while(iterator.hasNext()) {
-      // get each Entry individually
-      Map.Entry city = 
-        (Map.Entry)iterator.next();
-      // print out the entry's key and value
-      System.out.print(
-        "The key is: "+ city.getKey() +
-        " and the value is: " + city.getValue());
-      }
-   }
+        // "put" a few capital cities with their population
+        capitalCityPopulations.put("Ottawa", 1077900);
+        capitalCityPopulations.put("Lima", 11204000);
+        capitalCityPopulations.put("Nairobi", 5541172);
+
+        // demonstrate "getting" an
+        // element out of the HashMap
+        int limaPopulation = capitalCityPopulations.get("Lima");
+        System.out.println(limaPopulation);
+
+        // demonstrate iterating through
+        // the entries of a HashMap
+        Set entries = capitalCityPopulations.entrySet();
+        Iterator iterator = entries.iterator();
+        // loop through the entries
+        while(iterator.hasNext()) {
+        // get each Entry individually
+        Map.Entry city = (Map.Entry)iterator.next();
+        // print out the entry's key and value
+        System.out.print(
+            "The key is: "+ city.getKey() + " and the value is: " + city.getValue());
+        }
+    }
 }
