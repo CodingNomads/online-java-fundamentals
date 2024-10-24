7024ac2f9455d9fd96228aede18c0f8e1bde303b
CodingNomads Dev <ryan@codingnomads.co>
2024-10-24 15:09:39 -0700

    Update Exercise_01.java

--- src/labs_examples/datastructures/queue/labs/Exercise_01.java
+++ src/labs_examples/datastructures/queue/labs/Exercise_01.java
@@ -3,7 +3,7 @@ package labs_examples.datastructures.queue.labs;
 /**
  *      Demonstrate your mastery of Java's built-in Queue interface. You'll need to do a little 
  *      research here. What classes can you import that implement the Queue interface? Find one
- *      that you'd like to explore and use that class for this exercise. 
+ *      that you'd like to explore and use for this exercise. 
  *      https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/Queue.html
  *
  *      1) instantiate a new Queue
