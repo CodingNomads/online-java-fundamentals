10903250c5021f18f023d52483e0b564fddb9aa0
CodingNomads Dev <ryan@codingnomads.co>
2024-10-24 15:08:29 -0700

    Update Exercise_01.java

--- src/labs_examples/datastructures/queue/labs/Exercise_01.java
+++ src/labs_examples/datastructures/queue/labs/Exercise_01.java
@@ -1,9 +1,11 @@
 package labs_examples.datastructures.queue.labs;
 
 /**
- *      Demonstrate your mastery of Java's (sun's) built-in Queue class (as seen in the import statement
- *      above) by completing the following:
+ *      Demonstrate your mastery of Java's built-in Queue interface. You'll need to do a little 
+ *      research here. What classes can you import that implement the Queue interface? Find one
+ *      that you'd like to explore and use that class for this exercise. 
+ *      https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/Queue.html
  *
  *      1) instantiate a new Queue
- *      2) demonstrate the use of EVERY method in the Queue class - there are 7
+ *      2) demonstrate the use of EVERY method in the Queue class you're implementing 
  */
