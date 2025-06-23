f8fdd4fb23a1b1bd6f61105319486e283fad5ae8
CodingNomads Dev <ryan@codingnomads.co>
2025-06-23 15:34:59 -0700

    Update Exercise_03.java

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -3,6 +3,6 @@ package labs_examples.input_output.labs;
 /**
  * Input/Output Exercise 3: 
  *
- *    1) Demonstrate the use of the DataInputStream and DataOutputStream
+ *    1) Demonstrate the use of the DataOutputStream and DataInputStream
  *
  */
